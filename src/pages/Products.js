60e339d09c7f44142e1b988387e5dce83df89005
Mehmet <mathdenizi@gmail.com>
2022-10-23 14:23:30 +0200

    croisant

---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+const Products = () => {
+  return <h1>Hos geldiniz</h1>;
+};
+
+export default Products;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = () => {
+  return <h1>Hos geldiniz</h1>;
+};
+
+export default Welcome;
