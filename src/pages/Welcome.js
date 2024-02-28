e9d3b452bfa2b99297a9d33bfbef7a065de822bb
Mehmet <mathdenizi@gmail.com>
2024-02-28 04:15:23 +0100

    end of project, if u catch

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom";
 const Welcome = () => {
-  return <h1>Hos geldiniz</h1>;
+  return (
+    <section>
+      <h1>Hos geldiniz</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user</p>
+        <p1> write component or directly say hello here</p1>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
